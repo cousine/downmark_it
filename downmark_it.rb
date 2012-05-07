9516604a156c3b7de4167e54688054aab9d4f4c4
Omar Mekky <omar.mekky@mashsolvents.com>
2012-05-07 23:58:57 +0200

    closes #2: removed syntax error

--- downmark_it.rb
+++ downmark_it.rb
@@ -3,14 +3,14 @@
 #
 # =Motivation
 # While working on our company's new CMS, I needed to parse HTML back to markdown and surprisngly there wasn't any solution that could fit our enviroment, so I decided to make my own and share it :)
-# 
+#
 # =Usage
 # Make sure you install Hpricot[http://github.com/hpricot/hpricot/] first, then require the library in your application, if you are using the library in a rails application, just place it in your lib folder, then use this method to convert HTML into markdown.
 #  markdown = DownmarkIt.to_markdown(html)
-# 
+#
 # =Features
 # This library supports variable header tags, horizontal rulers, emphasis, strong, links, images, blockquotes, code, unordered lists(nested) and ordered lists(nested)
-# 
+#
 # =WARNING
 # Currently DownmarkIt does not support ul tags inside ol tags or vice versa, maybe in the future i will add it ;)
 #
@@ -22,7 +22,7 @@ module DownmarkIt
 	# TODO: Add nested unordered lists inside ordered list and vice versa support
 	def self.to_markdown(html)
 		raw = Hpricot(html.gsub(/(\n|\r|\t)/, ""))
-		
+
 		# headers
 		(raw/"/<h\d>/").each do |header|
 			if(header.name.match(/^h\d$/))
@@ -30,26 +30,26 @@ module DownmarkIt
 				header.swap("#{"#" * header_level} #{header.inner_html}\r\n")
 			end
 		end
-		
+
 		# horizontal rulers
 		(raw/"hr").each do |hruler|
 			hruler.swap("\r\n---\r\n")
 		end
-		
+
 		# emphasis
 		(raw/"em").each do |em|
 			if(em.name == "em")
 				em.swap("_#{em.inner_html}_")
 			end
 		end
-		
+
 		# strong
 		(raw/"strong").each do |strong|
 			if(strong.name == "strong")
 				strong.swap("**#{strong.inner_html}**")
 			end
 		end
-		
+
 		# links (anchors)
 		(raw/"a").each do |anchor|
 			if(anchor.name=="a")
@@ -60,26 +60,26 @@ module DownmarkIt
 				end
 			end
 		end
-		
+
 		# image
 		(raw/"img").each do |image|
 			image.swap("![#{image['alt']}](#{image['src']}#{" \"#{image['title']}\"" if image['title']})")
 		end
-		
+
 		# blockquote
 		(raw/"blockquote").each do |qoute|
 			if qoute.name == "blockquote"
 				qoute.swap("> #{nested_qoute(qoute)}")
 			end
 		end
-		
+
 		# code
 		(raw/"code").each do |code|
 			if code.name == "code"
 				code.swap("``#{code.inner_html}``")
 			end
 		end
-		
+
 		# unordered list
 		(raw/"ul").each do |ul|
 			if ul.name == "ul"
@@ -97,7 +97,7 @@ module DownmarkIt
 				ul.swap("#{ul.inner_html}")
 			end
 		end
-		
+
 		# ordered list
 		(raw/"ol").each do |ol|
 			if ol.name == "ol"
@@ -118,15 +118,15 @@ module DownmarkIt
 		end
 
 		# lines
-		(raw /"p").each do |p|
+		(raw/"p").each do |p|
 			if p.name == "p"
 				p.swap("\r\n#{p.inner_text}\r\n")
 			end
 		end
-		
+
 		return raw.to_s
 	end
-	
+
 	private
 	def self.nested_qoute(qoute)
 		nqoute = qoute.at("blockquote")
@@ -137,7 +137,7 @@ module DownmarkIt
 			qoute.inner_html
 		end
 	end
-	
+
 	def self.nested_ul(li, level)
 		ul = li.at("ul")
 		unless(ul.nil?)
@@ -146,7 +146,7 @@ module DownmarkIt
 			li.inner_html
 		end
 	end
-	
+
 	def self.nested_uli(li, level)
 		nli = li.at("li")
 		unless(nli.nil?)
@@ -164,7 +164,7 @@ module DownmarkIt
 			li.inner_html
 		end
 	end
-	
+
 	def self.nested_ol(li, level)
 		ol = li.at("ol")
 		unless(ol.nil?)
@@ -173,7 +173,7 @@ module DownmarkIt
 			li.inner_html
 		end
 	end
-	
+
 	def self.nested_oli(li, level)
 		nli = li.at("li")
 		unless(nli.nil?)
@@ -192,6 +192,6 @@ module DownmarkIt
 			li.inner_html
 		end
 	end
-	
-			
+
+
 end
